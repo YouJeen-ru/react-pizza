d75b4da9c66a09509c766a6cac3a458d9a504be5
YouJeen-ru <1.rouch@mail.ru>
2020-12-09 10:15:29 +0300

    use fetch add cloud database

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {Header} from "./components";
 import {Home, Cart} from "./pages";
 import {Route} from "react-router-dom";
@@ -6,12 +6,19 @@ import {Route} from "react-router-dom";
 
 
 function App() {
+    const[pizzas, setPizzas] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:3000/db.json').then((resp) => resp.json()).then(json => {
+            setPizzas(json.pizzas)
+        })
+    }, [])
     return (
         <>
             <div className="wrapper">
                 <Header/>
                 <div className="content">
-                    <Route path='/' component={Home} exact/>
+                    <Route path='/' render={() => <Home items={pizzas}/>} exact/>
                     <Route path='/cart' component={Cart} exact/>
                 </div>
             </div>
--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,8 @@ import Header from "./Header";
 import Button from "./Button";
 import Categories from "./Categories";
 import SortPopup from "./SortPopup";
+import PizzaBlock from "./PizzaBlock";
 
 
 
-export { Header, Button, Categories, SortPopup }
+export { Header, Button, Categories, SortPopup, PizzaBlock }
