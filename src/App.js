4a51d83e5d2a592b87e61aa63dd557fc56484b8d
YouJeen-ru <1.rouch@mail.ru>
2020-12-13 18:58:22 +0300

    add project json-server

--- src/App.js
+++ src/App.js
@@ -12,8 +12,9 @@ function App() {
     const dispatch = useDispatch()
 
     useEffect(() => {
-        axios.get('http://localhost:3000/db.json').then(({ data }) => {
-            dispatch(setPizzas(data.pizzas))
+        // Перенести в Redux и подключить redux-thunk
+        axios.get('http://localhost:3001/pizzas').then(({ data }) => {
+            dispatch(setPizzas(data))
         })
     }, [])
     return (
